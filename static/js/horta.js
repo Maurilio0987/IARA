34990a97ab32b1943cbefabe8ec667e7cda0795b
__Maurilio__ <116044148+Maurilio0987@users.noreply.github.com>
2025-04-29 09:20:51 -0300

    Add files via upload

--- static/js/horta.js
+++ static/js/horta.js
@@ -1,3 +1,35 @@
+function getChaveHorta() {
+  const partes = window.location.pathname.split('/');
+  return partes[partes.length - 1];
+}
+
+function atualizar_consumo() {
+  const chave = getChaveHorta();
+  fetch(`/consumo/${chave}`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    return response.json(); // ou response.text() se for texto
+  })
+  .then(data => {
+     console.log(data);
+	 let elemento_eto = document.getElementById("eto");
+	 let elemento_etc = document.getElementById("etc");
+	 let elemento_volume = document.getElementById("volume");
+
+
+	 elemento_eto.innerHTML = " " + data["eto"];
+	 elemento_etc.innerHTML = " " + data["etc"];
+	 elemento_volume.innerHTML = " " + data["volume"];
+	})
+
+  .catch(error => {
+    console.error('Erro ao buscar os dados:', error);
+  });
+}
+
+
 function atualizar_estacao() {
    fetch('/estacao')
   .then(response => {
@@ -22,9 +54,9 @@ function atualizar_estacao() {
   .catch(error => {
     console.error('Erro ao buscar os dados:', error);
   });
- 
 }
 
+atualizar_consumo();
 atualizar_estacao();
 
 setInterval(atualizar_estacao, 900000);
