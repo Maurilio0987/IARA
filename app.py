145e0f648027f1dd549c29df0511ee201c1013d0
__Maurilio__ <116044148+Maurilio0987@users.noreply.github.com>
2025-05-13 09:41:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -434,9 +434,9 @@ def adicionar_solo():
 
 
 
-app.run(debug=True, host="localhost", port=80)
+#app.run(debug=True, host="localhost", port=80)
 
 
-#if __name__ == "__main__":
-#    port = int(os.environ.get("PORT", 5000))
-#    app.run(host="0.0.0.0", port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
