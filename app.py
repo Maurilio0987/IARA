e86336d124c89f24e7c4dafb9fd11d1efd916930
__Maurilio__ <116044148+Maurilio0987@users.noreply.github.com>
2025-05-19 11:39:41 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -159,11 +159,11 @@ def atualizar_volumes():
 
 
 
-scheduler = BackgroundScheduler(timezone=timezone("America/Sao_Paulo"))
-scheduler.add_job(atualizar_volumes, "cron", minute=0, id="volume_por_hora")
-scheduler.add_job(db.zerar_volumes, "cron", hour=0, minute=0, second=0, id="zerar_volumes_diario")
-scheduler.add_job(db.atualizar_hortas, "cron", hour=0, minute=0, second=0, id="atualizar_hortas_diario")
-scheduler.start()
+#scheduler = BackgroundScheduler(timezone=timezone("America/Sao_Paulo"))
+#scheduler.add_job(atualizar_volumes, "cron", minute=0, id="volume_por_hora")
+#scheduler.add_job(db.zerar_volumes, "cron", hour=0, minute=0, second=0, id="zerar_volumes_diario")
+#scheduler.add_job(db.atualizar_hortas, "cron", hour=0, minute=0, second=0, id="atualizar_hortas_diario")
+#scheduler.start()
 
 
 
