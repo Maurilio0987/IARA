1d43b7de4e0298cdd8d18a38e091e6134bccc755
__Maurilio__ <116044148+Maurilio0987@users.noreply.github.com>
2025-07-07 10:44:01 -0300

    Add files via upload

--- static/js/horta.js
+++ static/js/horta.js
@@ -127,31 +127,6 @@ function atualizar_consumo24() {
 }
 
 
-function atualizar_estacao() {
-   fetch('/estacao')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`Erro na requisição: ${response.status}`);
-    }
-    return response.json(); // ou response.text() se for texto
-  })
-  .then(data => {
-     console.log(data);
-	 let elemento_temperatura = document.getElementById("temperatura");
-	 let elemento_umidade = document.getElementById("umidade");
-	 let elemento_solar = document.getElementById("radiacao");
-	 let elemento_vento = document.getElementById("vento");
-	 
-	 elemento_temperatura.innerHTML = " " + data["temperatura"];
-	 elemento_umidade.innerHTML = " " + data["umidade"];
-	 elemento_solar.innerHTML = " " + data["radiacao_solar"];
-	 elemento_vento.innerHTML = " " + data["vento"];
-	})
-  
-  .catch(error => {
-    console.error('Erro ao buscar os dados:', error);
-  });
-}
 
 function animateVolume(element, finalValue) {
 	const span = element;
@@ -171,9 +146,7 @@ function animateVolume(element, finalValue) {
 
 		
 atualizar_consumo();
-atualizar_estacao();
 atualizar_historico();
 atualizar_consumo24();
 
-setInterval(atualizar_estacao, 900000);
 
